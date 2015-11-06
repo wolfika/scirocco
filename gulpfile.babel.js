f38491774aef3e0e426e4dd0596c6a3024ed51f0
wolfika <farkas.mate38@gmail.com>
2015-11-06 20:54:29 +0100

    chore(gulp): add gulpfile

---
+++ gulpfile.babel.js
@@ -0,0 +1,22 @@
+import gulp from 'gulp';
+import loadPlugins from 'gulp-load-plugins';
+import config from './build.config';
+
+const tasks = require('require-dir')('./gulp');
+
+const $ = loadPlugins({
+  pattern: [
+    'del',
+    'gulp-*',
+    'wiredep',
+  ],
+});
+
+for (let key in tasks) {
+  tasks[key].run(gulp, $, config);
+}
+
+gulp.task('default', [
+  'lint',
+  'build',
+]);
